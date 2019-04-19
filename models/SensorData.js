3f7a1535d304ba7cb337c1483a9c3957cdc5ab5b
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-04-19 21:32:55 +0300

    Sequelizer and models have been implemented.

---
+++ models/SensorData.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const Model = Sequelize.Model;
+
+/**
+ * @class SensorData
+ * @extends {Model}
+ */
+class SensorData extends Model {};
+
+module.exports.init = (sequelize) => {
+  SensorData.init({
+    temperature: {
+      type: Sequelize.FLOAT,
+    },
+    humidity: {
+      type: Sequelize.FLOAT,
+    },
+    sensor_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'SensorData',
+  });
+
+  return SensorData;
+};
---
+++ models/db.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const connString = process.env.DATABASE_URL || 'postgres://postgres:development@postgres:5432/weatherdash';
+
+// @ts-ignore
+const sequelize = new Sequelize(connString);
+
+module.exports = sequelize;
