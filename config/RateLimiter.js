4faf3e429c03d556b885027477b656548a7d549a
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-04-19 21:32:33 +0300

    Added ratelimiter and ratelimiter config

---
+++ config/RateLimiter.js
@@ -0,0 +1,9 @@
+module.exports = {
+  points: 10, // Number of points
+  duration: 1, // Per second(s)
+
+  // Custom
+  execEvenly: false, // Do not delay actions evenly
+  blockDuration: 1, // Do not block if consumed more than points
+  keyPrefix: 'apiLimiter', // must be unique for limiters with different purpose
+}
---
+++ middlewares/rateLimiterRedis.js
@@ -0,0 +1,28 @@
+const {
+  RateLimiterRedis
+} = require('rate-limiter-flexible');
+
+module.exports = (app, redis) => {
+
+  const rateLimiterOpts = require('../config/RateLimiter');
+  rateLimiterOpts.redis = redis;
+
+  // @ts-ignore
+  const rateLimiter = new RateLimiterRedis(rateLimiterOpts);
+
+  return (req, res, next) => {
+    rateLimiter.consume(req.ip)
+      .then(() => {
+        next();
+      })
+      .catch(() => {
+        const resp = {
+          statusCode: 429,
+          error: 'Too Many Requests',
+          message: 'You are sending too many requests per second, Please slow down!'
+        }
+        res.status(429).json(resp);
+      });
+  };
+
+};
