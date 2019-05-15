0ca28ad4824f77a7d6ede42f7692f8cc1a18955e
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-05-15 17:38:17 +0300

    Quick fix added missing file

---
+++ src/models/helpers.js
@@ -0,0 +1,24 @@
+const sequelize = require('sequelize');
+
+module.exports.averageHour = [
+  [
+    // @ts-ignore
+    sequelize.fn('date_trunc', 'hour', sequelize.col('updatedAt')),
+    'timestamp',
+  ],
+  [
+    // @ts-ignore
+    sequelize.fn('AVG', sequelize.col('temperature')),
+    'avg_temp',
+  ],
+  [
+    // @ts-ignore
+    sequelize.fn('AVG', sequelize.col('humidity')),
+    'avg_humidity',
+  ],
+  'source_name',
+];
+
+module.exports.orderByTime = [[sequelize.col('timestamp'), 'DESC']];
+
+module.exports.groupByTime = ['timestamp', 'source_name'];
